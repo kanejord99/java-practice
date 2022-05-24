6b126f3f52b795946e7b5a861dd0090659202719
Jordon Kane <kanejord99@outlook.com>
2022-05-23 20:25:00 -0400

    TwoSum

---
+++ TwoSum/src/TwoSum.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+	public int[] twoSum(int[] nums, int target) {
+		Map<Integer, Integer> num_map = new HashMap<Integer, Integer>();
+		
+		for (int i=0; i<nums.length; i++) {
+			int complement = target - nums[i];
+			
+			if (num_map.containsKey(complement)) {
+				return new int[] {num_map.get(complement), i};
+			}
+			
+			num_map.put(nums[i], i);
+		}
+		
+		throw new IllegalArgumentException("Error.");
+	}	
+}
