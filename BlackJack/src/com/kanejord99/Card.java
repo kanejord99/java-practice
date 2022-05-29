cd2dbada28e030b1788d7011c863becdaf678a00
Jordon Kane <kanejord99@outlook.com>
2022-05-28 22:27:59 -0400

    changed numStr return value

--- BlackJack/src/com/kanejord99/Card.java
+++ BlackJack/src/com/kanejord99/Card.java
@@ -60,6 +60,6 @@ public class Card {
 			break;
 		}
 		
-		return numStr + " of " + mySuit.toString();																																													
+		return numStr + " of " + mySuit.toString() + ".";																																													
 	}
 }
